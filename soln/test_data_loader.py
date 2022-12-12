ebde5123a5ede538a0ce4b34ad6015b1e79aee98
Sean Paradiso <seanparadiso@gmail.com>
2022-12-12 16:37:10 -0500

    Push breaking test

--- soln/test_data_loader.py
+++ soln/test_data_loader.py
@@ -4,4 +4,4 @@ def test_load_csv_df():
     df = load_data("Test1.csv")
 
     # Make sure nothing has shifted under our feet wrt the Test1.csv input
-    assert len(df.index) == 20091, "Expected 20091 rows, got {}".format(len(df.index))
+    assert len(df.index) == 20090, "Expected 20091 rows, got {}".format(len(df.index))
