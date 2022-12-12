aecc70fd7851e55353e1705adf2136f258d850aa
Sean Paradiso <seanparadiso@gmail.com>
2022-12-12 16:45:50 -0500

    Fix flake8 issues

--- soln/test_data_loader.py
+++ soln/test_data_loader.py
@@ -1,7 +1,13 @@
 from soln.data_loader import load_data
 
+
 def test_load_csv_df():
+    """
+    Basic data loader tests.
+    """
+
     df = load_data("Test1.csv")
 
     # Make sure nothing has shifted under our feet wrt the Test1.csv input
-    assert len(df.index) == 20090, "Expected 20091 rows, got {}".format(len(df.index))
+    assert len(df.index) == 20090, \
+           "Expected 20091 rows, got {}".format(len(df.index))
