9f65fcffac39b3ae7625022544844d49fce161c0
Sean Paradiso <seanparadiso@gmail.com>
2022-12-15 21:54:52 -0500

    Finish up data exploration, add a few cleaning and enchrichment steps to data loader

--- soln/data_loader.py
+++ soln/data_loader.py
@@ -1,17 +1,97 @@
 import pandas as pd
+import numpy as np
 
 
-def load_data(csv_fname):
+# Some useful decorators
+def operate_on_copy(fn):
     """
-    Load in csv test data to a pandas dataframe.
+    Any data manipulation should be done on a fresh copy of the dataframe.
     """
+    def inner(df, *args, **kwargs):
+        df_copy = df.copy()
+        return fn(df_copy, *args, **kwargs)
 
-    return pd.read_csv(csv_fname)
+    return inner
 
 
+@operate_on_copy
 def clean_df(initial_df):
     """
     Clean the input dataframe.
     """
 
-    return initial_df
+    return initial_df.copy()
+
+
+@operate_on_copy
+def trim_start(df, force_threshold=500):
+    """
+    Trim the trace to start t=0 with z-axis contact
+    """
+
+    # Don't overthink it
+    skip_idx = 0
+    for fz_value in df['fz_1'].to_numpy():
+
+        # Drop the first `skip_idx` rows
+        if fz_value > force_threshold:
+            t0 = df.loc[skip_idx, 't']
+            df.loc[:, 't'] -= t0
+
+            return df.iloc[skip_idx:]
+
+        skip_idx += 1
+
+    assert False, "trim_start couldn't find the start of the build!"
+
+
+@operate_on_copy
+def trim_end(df, force_threshold=50):
+    """
+    Trim the trace to end with the last z-axis pullback
+    """
+
+    # Don't overthink it
+    fz_np = df['fz_1'].to_numpy()
+    for i in range(1, len(fz_np)):
+        fz_value = fz_np[-i]
+
+        # Drop the last `i` rows
+        if fz_value > force_threshold:
+            return df.iloc[:-i]
+
+    assert False, "trim_end couldn't find the end of the build!"
+
+
+@operate_on_copy
+def add_force_magnitudes(df):
+    """
+    Add a column for |<fx, fy, fz>| (both R1 and R2).
+    """
+
+    for idx in [1, 2]:
+        x = df[f"fx_{idx}"]
+        y = df[f"fy_{idx}"]
+        z = df[f"fz_{idx}"]
+
+        df[f"f{idx}_magnitude"] = (x*x + y*y + z*z).map(np.sqrt)
+
+    return df
+
+
+def load_data(csv_fname, clean=False):
+    """
+    Load in csv data to a pandas dataframe, clean it, and (optionally)
+    add any enrichments that shouldn't be part of the modeling pipeline.
+    """
+
+    df = pd.read_csv(csv_fname)
+
+    if not clean:
+        return df
+
+    cleaned = df.pipe(trim_start)\
+                .pipe(trim_end)\
+                .pipe(add_force_magnitudes)
+
+    return cleaned
--- soln/test_data_loader.py
+++ soln/test_data_loader.py
@@ -6,8 +6,25 @@ def test_load_csv_df():
     Basic data loader tests.
     """
 
-    df = load_data("Test1.csv")
+    df = load_data("Test1.csv", False)
 
     # Make sure nothing has shifted under our feet wrt the Test1.csv input
     assert len(df.index) == 20091, \
-           "Expected 20091 rows, got {}".format(len(df.index))
+        "Expected 20091 rows, got {}".format(len(df.index))
+
+
+def test_clean_and_enrich():
+    df = load_data("Test1.csv", True)
+
+    # Make sure nothing has shifted under our feet wrt the Test1.csv input
+    # This is sensitive to the thresholds used in trim_start/trim_end
+    assert len(df.index) == 19848, \
+        "Expected 19848 rows, got {}".format(len(df.index))
+
+    # Make sure the trace is trimmed
+    assert df.iloc[0]['fz_1'] > 500
+    assert df.iloc[-1]['fz_1'] > 50
+
+    # Make sure we have the total magnitude columns we expect
+    assert "f1_magnitude" in df.columns and "f2_magnitude" in df.columns, \
+        "Missing force magnitude columns"
